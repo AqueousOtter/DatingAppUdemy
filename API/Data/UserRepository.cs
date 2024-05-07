9270022cb7d72084122dd6b05c642ddba54535ef
DustinB <Dustinbrown@thbred.com>
2024-05-07 17:23:15 -0400

    Add Repo

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using API.Data;
+using API.Entities;
+using API.Interfaces;
+
+namespace API;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _context;
+
+    public UserRepository(DataContext _context)
+    {
+        this._context = _context;
+    }
+    public Task<AppUser> GetUserByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AppUser> GetUserByUsernameAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
